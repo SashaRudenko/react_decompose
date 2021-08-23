e2598fb8334e813cf156b7bba82517aafb5428bd
SashaRudenko <for_london@ukr.net>
2021-08-23 15:37:58 +0300

    Updates

---
+++ precache-manifest.54be2ec5d0c05933b236c1b12276193b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971b753245ec534a78c55525938e55ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "882474e857ccdc7504aa",
+    "url": "./static/css/main.e35b2da5.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "882474e857ccdc7504aa",
+    "url": "./static/js/main.5941fbfb.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
